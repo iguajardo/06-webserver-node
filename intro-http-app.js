c0b5c0df882b3a11e04a0f01a736d7b5ace8ff70
Italo Guajardo <italog.penaloza@gmail.com>
2021-10-21 16:18:42 -0300

    create example of express usage

---
+++ intro-http-app.js
@@ -0,0 +1,38 @@
+// paquete http que permite crear un servidor http usando node
+// luego lo reemplazaremos con express
+const http = require('http');
+
+const port = 8080;
+
+// crea un servidor y lo levanta de inmediato, si hay un error se cae, por eso se usa nodemon
+http.createServer((request, response) => {
+
+    // Se puede hacer un console.log a la request, para ver la request http que se recibe
+
+
+    // response.writeHead(200, { 'Content-Type': 'application/json' });
+    response.setHeader('Content-Disposition', 'attachment; filename=list.csv');
+    response.writeHead(200, { 'Content-Type': 'application/csv' });
+
+    // const persona = {
+    //     id: 1,
+    //     nombre: 'Italo'
+    // };
+
+    // para enviar un body, se hace con write:
+    // escribir distintos write, concatena el contenido al final
+    response.write('id, nombre\n');
+    response.write('1, Italo\n');
+    response.write('2, Nati\n');
+    response.write('3, Ami\n');
+    response.write('4, Black\n');
+
+    // se debe terminar la respuesta para enviarla, o queda en espera.
+    // además, la respuesta solo manda el text sin ninguna otra opción, como indicar que es un JSON
+    // esto pasa porque por defecto uno envía en el header un "connection": "keep-alive", y quedará
+    // pendiente con la conexión abierta hasta recibir una respuesta del servidor
+    response.end();
+
+}).listen(port); // abre el puerto
+
+console.log('Escuchando el puerto', port);
